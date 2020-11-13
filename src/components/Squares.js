537615df14c5daf59c505bb907cc5de7e28b9bad
Nick Watson <ngwatson79@yahoo.com>
2020-11-13 12:19:59 -0500

    squares completed

--- src/components/Squares.js
+++ src/components/Squares.js
@@ -32,7 +32,7 @@ export default function Squares() {
     // It should return a string containing the class name of 'active', if the id passed
     // as the argument matches the active square in state, empty string otherwise.
     // Right-click and "inspect element" on the square to see its effect.
-    return id === activeSquare ? "active" : "";
+    return id === activeSquare ? " active" : "";
   };
 
   const markActive = id => {
@@ -40,9 +40,12 @@ export default function Squares() {
     // Set the id argument to become the active id in state
     // (unless it already is, in which case we should reset
     // the currently active square id back to initial state).
-    // if (id === activeSquare)
+    
+      id === activeSquare ? setActiveSquare(!id) : setActiveSquare(id);
+    
   };
-
+  // console.log("square outside", squares)
+  // console.log("a outside", activeSquare)
   return (
     <div className='widget-squares container'>
       <h2>Squares</h2>
